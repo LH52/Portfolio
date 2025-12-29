1d003bc5119e10f0ce274afe6402ce6965fbc8ec
LH52 <louayhelou52@gmail.com>
2025-12-29 10:36:39 +0400

    Replaced previous project. Created React + Vite project. Created top menu bar.

---
+++ src/TopBar.tsx
@@ -0,0 +1,30 @@
+import './topBar.css'
+
+function TopBar() {
+
+  return (
+    <>
+    
+        <div id="topBar">
+            
+            <div>
+                <p style={{fontWeight: 'bold', fontSize: '2em'}}>Louay Helou</p>
+            </div>
+            <div style={{display: 'flex', alignItems: 'center', gap: '15px', fontSize: '1.3em'}}>
+                <p>Home</p>
+                <p>About</p>
+                <p>Skills</p>
+                <p>Work</p>
+                <p>Education</p>
+                <p>Experience</p>
+                <p>Contact</p> 
+            </div>
+            
+        </div>
+
+     
+    </>
+  )
+}
+
+export default TopBar
